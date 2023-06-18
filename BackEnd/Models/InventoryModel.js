cb80e2fd987a33f25f64291955896b9cf12f3615
Mohammad Momin <mominmohammad111@gmail.com>
2023-06-19 00:48:32 +0530

    Created API to edit and delete inventory items

--- BackEnd/Models/InventoryModel.js
+++ BackEnd/Models/InventoryModel.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 
 
 const inventorySchema = mongoose.Schema({
+    title : String,
     km_on_odometer: Number,
-    major_scratches: Number,
+    major_scratches: String,
     original_paint: String,
     number_of_accidents_report:Number,
     number_of_previous_buyers: Number,
     registeration_place : String,
-    image : String
+    image_url : String,
+    desc:String,
+    user_email : String
 });
 
 const InventoryModel = mongoose.model('marketplace_inventory',inventorySchema);
--- BackEnd/Routes/InventoryRoutes.js
+++ BackEnd/Routes/InventoryRoutes.js
@@ -8,32 +8,55 @@ InventoryRouter.get("/",async(req,res)=>{
     res.send({msg:inventory});
 });
 
-InventoryRouter.post("/",async(req,res)=>{
+InventoryRouter.post("/addCar",async(req,res)=>{
     let email = req.headers.email;
 
     const {
+        title,
         km_on_odometer,
         major_scratches,
         original_paint,
         number_of_accidents_report,
         number_of_previous_buyers,
         registeration_place,
-        image
+        image_url,
+        desc
     } = req.body;
 
     let newCar = await new InventoryModel({
+        title,
         km_on_odometer,
         major_scratches,
         original_paint,
         number_of_accidents_report,
         number_of_previous_buyers,
         registeration_place,
-        image,
-        user : email
+        image_url,
+        desc,
+        user_email : email
     });
     await newCar.save();
 
     res.send({msg:"Added Item to inventory",item:newCar});
 });
 
-module.exports = InventoryRouter;
+InventoryRouter.delete("/delete/:id",async(req,res)=>{
+    let id  = req.params.id;
+
+    await InventoryModel.findOneAndDelete({_id:id});
+
+    res.send({msg:"Item deleted",item:inventory})
+
+});
+
+InventoryRouter.patch("/update/:id",async(req,res)=>{
+    let id  = req.params.id;
+    let payload = req.body;
+console.log(payload)
+     await InventoryModel.findOneAndUpdate({_id:id},{$set:payload},{new:true});
+
+    res.send({msg:"Item Updated"})
+
+});
+
+module.exports = InventoryRouter; 
